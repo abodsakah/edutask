a59acbd5ad25907f5f3ef148a63c04b096011024
abodsakka <abodsakka2001@gmail.com>
2022-06-26 13:01:41 +0200

    updated test_auth

--- backend/test/unit/test_auth.py
+++ backend/test/unit/test_auth.py
@@ -5,7 +5,7 @@ from src.util.helpers import hasAttribute, ValidationHelper
 from src.controllers.usercontroller import UserController
 from src.util.daos import getDao
 
-# controller = UserController(getDao(collection_name='user'))
+controller = UserController(getDao(collection_name='user'))
 
 # tests for the hasAttribute method
 # @pytest.mark.demo
@@ -54,16 +54,41 @@ def test_get_user_info_by_mail(capsys):
 
     captured = capsys.readouterr()
 
-    data = {'firstName': 'Janel', 'lastName': 'Doe', 'email': 'janel.doe@mail.com'}
-    user = controller.create(data)
+    data = {'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe@mail.com'}
+    controller.create(data)
 
-    # Try to login with a valid email adress
-    user = controller.get_user_by_email('janel.doe@mail.com')
-    assert user['firstName'] == 'Janel'
+    # # Try to login with a valid email adress
+    # user = controller.get_user_by_email('janel.doe@mail.com')
+    # assert user['firstName'] == 'Janel'
 
-    data = {'firstName': 'Janel', 'lastName': 'Doe', 'email': 'janel.doe@mail.com'}
-    user = controller.create(data)
+    # data = {'firstName': 'Janel', 'lastName': 'Doe', 'email': 'janel.doe@mail.com'}
+    # user = controller.create(data)
 
-    # Try to login with a valid email adress
-    user = controller.get_user_by_email('janel.doe@mail.com')
+    # # Try to login with a valid email adress
+    # user = controller.get_user_by_email('janel.doe@mail.com')
+    # assert 'Error: more than one user found with mail ' in captured.out
+
+
+    # Loging in with a unique, valid email address that is associated to a user
+    # Should return: user object
+    user = controller.get_user_by_email("jane.doe@mail.com")
+    assert user['firstName'] == 'Jane'
+
+    # valid email address that is not associated to a user
+    # Should return: None
+    user = controller.get_user_by_email("jane@mail.com")
+    assert user is None
+
+    # Invalid format email address
+    # Should return: ValueError
+    with pytest.raises(ValueError):
+        user = controller.get_user_by_email("jane.doe@mail")
+
+
+    data = {'firstName': 'Janel', 'lastName': 'Doel', 'email': 'jane.doe@mail.com'}
+    controller.create(data)
+
+    # Loging in with a none unique, valid email address that is associated to multiple users
+    # Should return: IndexError
+    user = controller.get_user_by_email("jane.doe@mail.com")
     assert 'Error: more than one user found with mail ' in captured.out
